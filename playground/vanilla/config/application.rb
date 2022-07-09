be9545e80609ffafc04c9be1f27a1219377ce69b
Maximo Mussini <maximomussini@gmail.com>
2022-07-09 15:21:41 -0300

    chore: example rails setup

---
+++ playground/vanilla/config/application.rb
@@ -0,0 +1,13 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    config.autoloader = :zeitwerk
+  end
+end

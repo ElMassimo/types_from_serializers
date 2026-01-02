f71b97d9a48e3d6ecf20ba84ec7d402a26f6992e
Maximo Mussini <maximomussini@gmail.com>
2026-01-02 14:15:17 -0300

    chore: fix rails 8 enum syntax

--- playground/vanilla/app/models/song.rb
+++ playground/vanilla/app/models/song.rb
@@ -2,6 +2,11 @@ class Song < ApplicationRecord
   belongs_to :composer
   has_many :video_clips
 
-  enum genre: {fingerstyle: "fingerstyle", rock: "rock", classical: "classical"}
-  enum tempo: %w[slow medium fast]
+  if Rails::VERSION::MAJOR >= 7
+    enum :genre, fingerstyle: "fingerstyle", rock: "rock", classical: "classical"
+    enum :tempo, %w[slow medium fast]
+  else
+    enum genre: {fingerstyle: "fingerstyle", rock: "rock", classical: "classical"}
+    enum tempo: %w[slow medium fast]
+  end
 end
